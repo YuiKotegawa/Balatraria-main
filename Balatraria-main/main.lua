9eca20e76d471e0c2ea17b2c771fa8542379e01f
Senechal_Scott <100836619+YuiKotegawa@users.noreply.github.com>
2025-09-29 21:51:43 +0200

    Joker test (temporaire)
    Updated the mod header and restructured Joker definitions.

--- Balatraria-main/main.lua
+++ Balatraria-main/main.lua
@@ -1,126 +1,58 @@
 --- STEAMODDED HEADER
 --- MOD_NAME: Balatraria
---- MOD_ID: BALATRARIA
---- MOD_AUTHOR: [Roux2scours, Natoke]
---- MOD_DESCRIPTION: Terraria mod for balatro, with new jokers, consumables, and more.
+--- MOD_ID: balatraria
+--- MOD_AUTHOR: Natoke, Roux2scours
+--- MOD_DESCRIPTION: A terraria mod for balatro, that adds new jokers, consumables and more!
+--- MOD_VERSION: 1.0.0
 --- PREFIX: balatraria
 
--- Base SteamOdded/SMODS pour Balatraria
+----------------------------------------------
+------------MOD CODE -------------------------
+
 SMODS.Atlas {
-    key = "Jokers1",
-    path = "Dual_slime.png",
-    px = 71,
-    py = 95,
+    key = "Jokers",
+    name = "Jokers1", 
+    path = "Jokers1.png",
+    px = 71, 
+    py = 95   
 }
 
--- Joker 1 : King Slime
+local xchips = 2
+
 SMODS.Joker {
-    key = "king_slime",
-    loc_txt = {
-        name = "King Slime",
-        text = {
-            "If played hand contains a King",
-            "{X:mult,C:white}X2 Mult",
-        },
-    },
-    atlas = "Jokers1",
+    key = "kingslime",
+    loc_vars = function(self, info_queue, center)
+        return {vars = {xchips}}
+    end,
+    atlas = "Jokers",
     rarity = 2,
     cost = 5,
     unlocked = true,
-    discovered = true,
     blueprint_compat = true,
-    eternal_compat = false,
-    perishable_compat = false,
     pos = { x = 0, y = 0 },
-    config = {
-        extra = {
-            Xmult = 2
-        },
-    },
-    loc_vars = function(self, info_queue, center)
-        return { vars = { center.ability.extra.Xmult } }
-    end,
     calculate = function(self, card, context)
-        if context.joker_main then
-            return {
-                card = card,
-                Xmult_mod = card.ability.extra.Xmult,
-                message = "X" .. card.ability.extra.Xmult,
-                colour = G.C.MULT
-            }
+        if context.cardarea == G.jokers and context.scoring_hand then
+            if context.joker_main then
+                local has_king = false
+                for i = 1, #context.scoring_hand do
+                    if context.scoring_hand[i]:get_id() == 13 then -- King = 13
+                        has_king = true
+                        break
+                    end
+                end
+                if has_king then
+                    return {
+                        message = "X" .. xchips .. " Chips!",
+                        Xchip_mod = xchips,
+                        colour = G.C.CHIPS,
+                        card = card
+                    }
+                end
+            end
         end
     end
 }
 
--- Joker 2 : Eye of Cthulhu
-SMODS.Joker {
-    key = "eye_cthulhu",
-    loc_txt = {
-        name = "Eye of Cthulhu",
-        text = {
-            "If played hand contains only Hearts",
-            "Gain {C:money}10${} at end of round",
-        },
-    },
-    atlas = "Jokers1",
-    rarity = 3,
-    cost = 10,
-    unlocked = true,
-    discovered = true,
-    blueprint_compat = true,
-    eternal_compat = false,
-    perishable_compat = false,
-    pos = { x = 1, y = 0 },
-    config = {},
-    calculate = function(self, card, context)
-        if context.joker_main and context.only_hearts then
-            return {
-                card = card,
-                dollar_bonus = 10,
-                message = "+10$",
-                colour = G.C.MONEY
-            }
-        end
-    end
-}
 
--- Joker 3 : Terra Blade
-SMODS.Joker {
-    key = "terra_blade",
-    loc_txt = {
-        name = "Terra Blade",
-        text = {
-            "If played hand contains a Straight",
-            "{X:mult,C:green}X3 Mult",
-        },
-    },
-    atlas = "Jokers1",
-    rarity = 4,
-    cost = 20,
-    unlocked = true,
-    discovered = true,
-    blueprint_compat = true,
-    eternal_compat = false,
-    perishable_compat = false,
-    pos = { x = 2, y = 0 },
-    config = {
-        extra = {
-            Xmult = 3
-        },
-    },
-    loc_vars = function(self, info_queue, center)
-        return { vars = { center.ability.extra.Xmult } }
-    end,
-
-    calculate = function(self, card, context)
-        if context.joker_main and context.straight then
-            return {
-                card = card,
-                Xmult_mod = card.ability.extra.Xmult,
-                message = "X" .. card.ability.extra.Xmult,
-                colour = G.C.GREEN
-            }
-        end
-    end
-}
-
+----------------------------------------------
+------------MOD CODE END----------------------
